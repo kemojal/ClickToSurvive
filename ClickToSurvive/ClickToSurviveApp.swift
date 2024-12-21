549c48d93087f5b4f4c6778dd63c07aa9271fccd
kemo <kemo.jallow@hannlync.com>
2024-12-22 01:34:05 +0800

    Initial Commit

---
+++ ClickToSurvive/ClickToSurviveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickToSurviveApp.swift
+//  ClickToSurvive
+//
+//  Created by Kemo Jallow on 2024/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ClickToSurviveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClickToSurviveTests/ClickToSurviveTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClickToSurviveTests.swift
+//  ClickToSurviveTests
+//
+//  Created by Kemo Jallow on 2024/12/22.
+//
+
+import Testing
+@testable import ClickToSurvive
+
+struct ClickToSurviveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
